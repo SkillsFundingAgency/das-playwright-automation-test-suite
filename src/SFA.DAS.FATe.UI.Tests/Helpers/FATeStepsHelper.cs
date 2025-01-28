68adca81038031297d52aa300752fd314adb6377
sudhakar.chinoor <sudhakar.chinoor@digital.education.gov.uk>
2025-01-28 12:11:50 +0000

    Added new FATe project

---
+++ src/SFA.DAS.FATe.UI.Tests/Helpers/FATeStepsHelper.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.FATe.UITests.Project.Tests.Pages;
+
+namespace SFA.DAS.FATe.UITests.Helpers
+{
+    public class FATeStepsHelper(ScenarioContext context)
+    {
+        public async Task<FATeHomePage> GoToFATeHomePage() => await new FATeHomePage(context).AcceptCookieAndAlert();
+
+    }
+}
---
+++ src/SFA.DAS.FATe.UI.Tests/Project/Tests/Pages/FATeHomePage.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.FATe.UITests.Project.Tests.Pages;
+
+namespace SFA.DAS.FATe.UITests.Project.Tests.Pages;
+
+public class FATeHomePage(ScenarioContext context) : FATeBasePage(context)
+{
+    public override async Task VerifyPage() => await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Find apprenticeship training for your apprentice");
+
+    public async Task<FATeHomePage> AcceptCookieAndAlert()
+    {
+        await page.GetByRole(AriaRole.Button, new() { Name = "Accept all cookies" }).ClickAsync();
+
+        await page.Locator(".das-cookie-banner__hide-button").ClickAsync();
+
+        return await VerifyPageAsync(() => new FATeHomePage(context));
+    }
+}
