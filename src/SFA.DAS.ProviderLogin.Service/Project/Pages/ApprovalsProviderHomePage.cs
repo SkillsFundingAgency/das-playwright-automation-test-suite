f3d73eabbe720c1c26d9df8e9937753184b1798b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-03-25 14:30:04 +0000

    re arranged provider homepage

--- src/SFA.DAS.ProviderLogin.Service/Project/Pages/ApprovalsProviderHomePage.cs
+++ src/SFA.DAS.ProviderLogin.Service/Project/Pages/ApprovalsProviderHomePage.cs
@@ -144,34 +144,7 @@ public partial class ProviderHomePage : InterimProviderBasePage
         return await VerifyPageAsync(() => new ProviderViewEmployerRequestsForTrainingPage(context));
     }
 
-    public async Task<ApimAccessDeniedPage>  NavigateToDeveloperAPIsPageGoesToApimAccessDenied()
-    {
-        await page.GetByRole(AriaRole.Link, new() { Name = "Developer APIs" }).ClickAsync();
-
-        return await VerifyPageAsync(() => new ApimAccessDeniedPage(context));
-    }
-
-    public async Task<ProviderAccessDeniedPage> GotoSelectJourneyPageGoesToAccessDenied()
-    {
-        await AddNewApprentices();
-
-        return await VerifyPageAsync(() => new ProviderAccessDeniedPage(context));
-    }
-
-    public async Task<ProviderAccessDeniedPage> GotoAddNewEmployerStartPageGoesToAccessDenied()
-    {
-        await ClickAddAnEmployerLink();
-
-        return await VerifyPageAsync(() => new ProviderAccessDeniedPage(context));
-    }
-
-    public async Task<ProviderAccessDeniedPage> GoToProviderGetFundingGoesToAccessDenied()
-    {
-        await ClickFundingLink();
-
-        return await VerifyPageAsync(() => new ProviderAccessDeniedPage(context));
-    }
-
+  
     private async Task AddNewApprentices() => await page.GetByRole(AriaRole.Link, new() { Name = "Add new apprentices" }).ClickAsync();
 }
 
--- src/SFA.DAS.ProviderLogin.Service/Project/Pages/ProviderHomePage.cs
+++ src/SFA.DAS.ProviderLogin.Service/Project/Pages/ProviderHomePage.cs
@@ -9,35 +9,6 @@ public partial class ProviderHomePage(ScenarioContext context) : InterimProvider
         await Assertions.Expect(page.Locator("#main-content")).ToContainTextAsync($"UKPRN: {ukprn}", new() { Timeout = 300000 });
     }
 
-    //protected static By AddNewApprenticesLink => By.LinkText("Add new apprentices");
-
-    //protected static By AddAnEmployerLink => By.LinkText("Add an employer");
-
-    //protected static By ViewEmployersAndManagePermissionsLink => By.LinkText("View employers and manage permissions");
-
-    //protected static By ProviderManageYourApprenticesLink => By.LinkText("Manage your apprentices");
-
-    //protected static By ProviderRecruitApprenticesLink => By.LinkText("Recruit apprentices");
-
-    //protected static By DeveloperAPIsLink => By.LinkText("Developer APIs");
-
-    //protected static By GetFundingLink => By.LinkText("Get funding for non-levy employers");
-
-    //protected static By ManageYourFundingLink => By.LinkText("Manage your funding reserved for non-levy employers");
-
-    //protected static By ManageEmployerInvitations => By.LinkText("Manage employer invitations");
-
-    //protected static By InviteEmployers => By.LinkText("Send invitation to employer");
-
-    //protected static By RecruitTrainees => By.LinkText("Recruit trainees");
-
-    //protected static By AppsIndicativeEarningsReport => By.LinkText("Apps Indicative earnings report");
-
-    //protected static By YourStandardsAndTrainingVenues => By.LinkText("Your standards and training venues");
-    //protected static By YourFeedback => By.LinkText("Your feedback");
-
-    //protected static By ViewEmployerRequestsForTraining => By.LinkText("View employer requests for training");
-
     public async Task ClickAddAnEmployerLink() => await page.GetByRole(AriaRole.Link, new() { Name = "Add an employer" }).ClickAsync();
 
     public async Task ClickFundingLink() => await page.GetByRole(AriaRole.Link, new() { Name = "Get funding for non-levy" }).ClickAsync();
