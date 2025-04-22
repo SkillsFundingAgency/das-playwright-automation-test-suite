0de5685a4ccaf0ec1a0893412f73f95746709c05
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-04-22 16:24:56 +0100

    fixed hooks

--- src/SFA.DAS.ManagingStandards.UITests/Project/ManagingStandardsConfigurationSetup.cs
+++ src/SFA.DAS.ManagingStandards.UITests/Project/ManagingStandardsConfigurationSetup.cs
@@ -5,6 +5,6 @@ namespace SFA.DAS.ManagingStandards.UITests.Project;
 [Binding]
 public class ManagingStandardsConfigurationSetup(ScenarioContext context)
 {
-    [BeforeScenario(Order = 2)]
+    [BeforeScenario(Order = 12)]
     public void SetUpManagingStandardsProjectConfiguration() => context.SetNonEasLoginUser(SetDfeAdminCredsHelper.SetDfeAdminCreds(context.Get<FrameworkList<DfeAdminUsers>>(), new AsAdminUser()));
 }
