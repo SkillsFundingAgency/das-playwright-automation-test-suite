40f51be7b4c8dd8080c6be752e2a5700fe1b4ecb
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-05-02 15:32:26 +0100

    skeleton approvals project.

---
+++ src/SFA.DAS.Approvals.UITests/Project/Tests/Steps/AP_Steps.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.ProviderLogin.Service.Project.Helpers;
+
+namespace SFA.DAS.Approvals.UITests.Project.Tests.Steps;
+
+[Binding]
+public class AP_Steps(ScenarioContext context)
+{
+    [Given(@"the provider logs into portal")]
+    public async Task GivenTheProviderLogsIntoPortal() => await new ProviderHomePageStepsHelper(context).GoToProviderHomePage(false);
+}
