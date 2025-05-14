676287bb9c409be5b1202b93414d35ccce51f6fd
Gabriel PT <98819464+GPeterThomas@users.noreply.github.com>
2025-05-14 14:44:42 +0100

    Added provider login

---
+++ src/SFA.DAS.FATe.UITests/Project/Tests/StepDefinitions/FAT_Provider.cs
@@ -0,0 +1,14 @@
+﻿using Polly;
+using SFA.DAS.FATe.UITests.Project.Tests.Pages;
+using SFA.DAS.ProviderLogin.Service.Project.Helpers;
+
+
+namespace SFA.DAS.FATe.UITests.Project.Tests.StepDefinitions
+{
+    [Binding]
+    public class FAT_Provider(ScenarioContext context)
+    {
+        [Given(@"the provider logs into portal")]
+        public async Task GivenTheProviderLogsIntoPortal() => await new ProviderHomePageStepsHelper(context).GoToProviderHomePage(false);
+    }
+}
