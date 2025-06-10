3e17d9dac2554ff291d402f4d6bab57b73fad285
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-06-10 11:51:28 +0100

    early connect project

---
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Helpers/EarlyConnectStepsHelper.cs
@@ -0,0 +1,30 @@
+﻿//using SFA.DAS.EarlyConnectForms.UITests.Project.Tests.Pages;
+
+//using static SFA.DAS.EarlyConnectForms.UITests.Project.Tests.Pages.ConfirmSurveySentPage;
+
+using SFA.DAS.EarlyConnectForms.UITests.Project.Tests.Pages;
+
+namespace SFA.DAS.EarlyConnectForms.UITests.Project.Helpers;
+
+public class EarlyConnectStepsHelper(ScenarioContext context)
+{
+    public async Task GoToEarlyConnectHomePage() => await new EarlyConnectHomePage(context).AcceptCookieAndAlert();
+    public async Task GoToEarlyConnectNorthEastAdvisorPage() => await new EarlyConnectHomePage(context).SelectNorthEast();
+    public async Task GoToEarlyConnectLancashireAdvisorPage() =>  await new EarlyConnectHomePage(context).SelectLancashire();
+    public async Task GoToEarlyConnectLondonAdvisorPage() => await new EarlyConnectHomePage(context).SelectLondon();
+    public async Task GoToEarlyConnectEmailPage() => await new EarlyConnectHomePage(context).GoToEmailAddressPage();
+    public async Task<EmailAuthCodePage> GoToAddUniqueEmailAddressPage() =>  await new EmailAddressPage(context).EnterNewEmailAddress();
+    public async Task<WhatsYourNamePage> GoToCheckEmailAuthCodePage() => await new EmailAuthCodePage(context).EnterValidAuthCode();
+    public async Task<DateOfBirthPage> GoToWhatYourNamePage() => await new WhatsYourNamePage(context).EnterFirstAndLastNames();
+    public async Task<PostcodePage> GoToWhatIsYourDateOfBirthPage() => await new DateOfBirthPage(context).EnterValidDateOfBirth();
+    public async Task<TelephonePage> GoToPostCodePage() => await new PostcodePage(context).EnterValidPostcode();
+    public async Task<AreasOfWorkInterestPage> GoToWhatYourTelephonePage() => await new TelephonePage(context).EnterValidTelephoneNumber();
+    public async Task<SchoolCollegePage> GoToAreasOfWorkInterestPage() => await new AreasOfWorkInterestPage(context).SelectAnyAreaInterestToYou();
+    public async Task<ApprenticeshipsLevelPage> GoToEnterNameOfSchoolCollegePage() => await new SchoolCollegePage(context).EnterInvalidSchoolOrCollegeName();
+    public async Task<ApprenticeshipsLevelPage> GoToNameOfSchoolCollegePage() => await new SchoolCollegePage(context).SearchValidSchoolOrCollegeName();
+    public async Task<HaveYouAppliedPage> GoToApprencticeshipLevelPage() => await new ApprenticeshipsLevelPage(context).SelectAnyApprenticeshipLevelInterestToYou();
+    public async Task<AreaOfEnglandPage> GoToHaveYouAppliedPage() => await new HaveYouAppliedPage(context).SelectAnyPastApplications();
+    public async Task<SupportPage> GoToAreaOfEnglandPage() => await new AreaOfEnglandPage(context).SelectYesForTheRightApprenticeship();
+    public async Task<CheckYourAnswerPage> GoToSupportPage() => await new SupportPage(context).SelectAnySupportThatAppliesToYou();
+    public async Task<ApplicantSurveySummitedPage> GoToCheckYourAnswerPage() => await new CheckYourAnswerPage(context).AcceptAndSubmitForm();
+}
---
+++ src/SFA.DAS.EarlyConnectForms.UITests/Project/Hooks.cs
@@ -0,0 +1,46 @@
+﻿namespace SFA.DAS.EarlyConnectForms.UITests.Project;
+
+[Binding]
+public class Hooks
+{
+    private readonly ObjectContext _objectContext;
+    private readonly DbConfig _dbConfig;
+    private readonly ScenarioContext _context;
+    private readonly EarlyConnectSqlHelper _sqlHelper;
+
+    public Hooks(ScenarioContext context)
+    {
+        _context = context;
+        _objectContext = context.Get<ObjectContext>();
+        _dbConfig = context.Get<DbConfig>();
+        _sqlHelper = new EarlyConnectSqlHelper(_objectContext, _dbConfig);
+    }
+
+    [BeforeScenario(Order = 31)]
+    public async Task SetUpHelpers()
+    {
+        var name = _sqlHelper.GetAnEducationalOrganisation();
+
+        var email = _context.Get<MailosaurUser>().GetEmailList().FirstOrDefault().Email;
+
+        var datahelper = new EarlyConnectDataHelper(email, name);
+
+        _context.Set(datahelper);
+
+        _objectContext.SetDebugInformation($"'{datahelper.Email}' is used");
+
+        var driver = _context.Get<Driver>();
+
+        var url = UrlConfig.EarlyConnect_BaseUrl;
+
+        _context.Get<ObjectContext>().SetDebugInformation(url);
+
+        await driver.Page.GotoAsync(url);
+    }
+
+    [AfterScenario(Order = 31)]
+    public async Task DeleteStudentData()
+    {
+        await _sqlHelper.DeleteStudentDataAndAnswersByEmail(_context.Get<EarlyConnectDataHelper>().Email);
+    }
+}
