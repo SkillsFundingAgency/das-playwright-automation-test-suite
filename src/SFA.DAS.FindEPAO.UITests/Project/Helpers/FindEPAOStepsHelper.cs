a9a66819b29ded39b3e68028bd8eecdbbc959656
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-07-11 11:26:21 +0100

    minor changes

--- src/SFA.DAS.FindEPAO.UITests/Project/Helpers/FindEPAOStepsHelper.cs
+++ src/SFA.DAS.FindEPAO.UITests/Project/Helpers/FindEPAOStepsHelper.cs
@@ -4,28 +4,28 @@ namespace SFA.DAS.FindEPAO.UITests.Project.Helpers;
 
 class FindEPAOStepsHelper(ScenarioContext context)
 {
-    public async Task<EPAOOrganisationsPage> SearchForApprenticeshipStandard(string standard = "")
+    public async Task<EPAOOrganisationsPage> SearchForApprenticeshipStandard(string standard)
     {
         var page = await FindEPAO();
         
         return await page.SearchForApprenticeshipStandardInSearchApprenticeshipTrainingCoursePage(standard);
     }
 
-    public async Task<ZeroAssessmentOrganisationsPage> SearchForApprenticeshipStandardWithNoEPAO(string standard = "")
+    public async Task<ZeroAssessmentOrganisationsPage> SearchForApprenticeshipStandardWithNoEPAO(string standard)
     {
         var page = await FindEPAO();
 
         return await page.SearchForApprenticeshipStandardWithNoEPAO(standard);
     }
 
-    public async Task<EPAOOrganisationDetailsPage> SearchForApprenticeshipStandardWithSingleEPAO(string standard = "")
+    public async Task<EPAOOrganisationDetailsPage> SearchForApprenticeshipStandardWithSingleEPAO(string standard)
     {
         var page = await FindEPAO();
 
         return await page.SearchForApprenticeshipStandardWithSingleEPAO(standard);
     }
 
-    public async Task<EPAOOrganisationsPage> SearchForIntegratedApprenticeshipStandard(string standard = "")
+    public async Task<EPAOOrganisationsPage> SearchForIntegratedApprenticeshipStandard(string standard)
     {
         var page = await FindEPAO();
 
