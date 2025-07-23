f922a34ae0396c6704958bb027425f3b8116126a
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2025-07-23 17:35:34 +0100

    refactoring

--- src/SFA.DAS.Approvals.UITests/Project/Helpers/DataHelpers/FileUpload/FileUploadHelper.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/DataHelpers/FileUpload/FileUploadHelper.cs
@@ -9,19 +9,8 @@ namespace SFA.DAS.Approvals.UITests.Project.Helpers.DataHelpers.FileUploadModel
 
         public FileUploadHelper(ScenarioContext _context) => context = _context;
 
-        //internal string CsvFileLocation2 => Path.GetFullPath(@"..\..\..\") + @"Project\CsvFiles\" + $"{context.ScenarioInfo.Title[..8]}_BulkUpload.csv";
+        internal string CsvFileLocation() => Path.GetFullPath(@"..\..\..\") + $"{context.ScenarioInfo.Title[..8]}_BulkUpload.csv";
 
-        internal string CsvFileLocation()
-        {
-            //var approvalsConfig = context.GetApprovalsConfig<ApprovalsConfig>();
-            //return Path.GetFullPath(@"..\..\..\") + approvalsConfig.BulkUploadFileLocation;
-
-            return Path.GetFullPath(@"..\..\..\") + $"{context.ScenarioInfo.Title[..8]}_BulkUpload.csv";
-
-            //var fileName = $"{context.ScenarioInfo.Title[..8]}_BulkUpload.csv";
-            //return Path.Combine(AppContext.BaseDirectory, "Project", "CsvFiles", fileName);
-
-        }
 
     }
 }
