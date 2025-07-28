7066a0f4ca0aadeea27958037d08795173e67a2b
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-07-28 15:37:30 +0100

    Aan employer test scenario migration (#68)
    * aan employer scenarios
    
    * updated steps
    
    * updated selector
    
    * updated ApprenticeMessagePage
    
    * employer test scenarios fix
    
    * minor review changes

---
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Models/NetworkEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Models
+{
+    public class NetworkEvent
+    {
+        public string EventTitle { get; set; }
+        
+    }
+
+    public class NetworkEventWithLocation : NetworkEvent
+    {
+        public string Location { get; set; }
+    }
+
+    public class NetworkEventWithOrdinal : NetworkEvent
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeAmbassadorNetwork.UITests/Project/Tests/Pages/AppEmpCommonPages/SearchNetworkEventsPage.cs
@@ -0,0 +1,103 @@
+﻿namespace SFA.DAS.ApprenticeAmbassadorNetwork.UITests.Project.Tests.Pages.AppEmpCommonPages;
+
+public class SearchNetworkEventsPage(ScenarioContext context) : SearchEventsBasePage(context)
+{
+    public override async Task VerifyPage()
+    {
+        await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Search network events");
+    }
+
+    //Clicking on last event so that the event would be for sure in the future
+    public async Task<EventPage> ClickOnLastEvent()
+    {
+        await FilterEventFromTomorrow();
+
+        await page.Locator(EventLink).Last.ClickAsync();
+
+        return new EventPage(context);
+    }
+
+    public new async Task FilterEventByOneMonth()
+    {
+        await base.FilterEventByOneMonth();
+    }
+
+    public async Task FilterEventsWithNoResults()
+    {
+        await EnterKeywordFilter(Guid.NewGuid().ToString());
+
+        await ApplyFilter();
+        
+    }
+
+    public new async Task FilterEventByEventFormat_InPerson()
+    {
+        await base.FilterEventByEventFormat_InPerson();
+        
+    }
+
+    public new async Task FilterEventByEventFormat_Online()
+    {
+        await base.FilterEventByEventFormat_Online();
+        
+    }
+
+    public new async Task FilterEventByEventFormat_Hybrid()
+    {
+        await base.FilterEventByEventFormat_Hybrid();
+        
+    }
+
+    public new async Task FilterEventByEventType_TrainingEvent()
+    {
+        await base.FilterEventByEventType_TrainingEvent();
+        
+    }
+
+    public new async Task FilterEventByEventRegion_London()
+    {
+        await base.FilterEventByEventRegion_London();
+        
+    }
+
+    public new async Task FilterEventByEventStatus_Cancelled()
+    {
+        await base.FilterEventByEventStatus_Cancelled();
+        
+    }
+
+    public new async Task ClearAllFilters()
+    {
+        await base.ClearAllFilters();
+        
+    }
+    public new async Task VerifyEventType_TrainingEvent_Filter()
+    {
+        await base.VerifyEventType_TrainingEvent_Filter();
+        
+    }
+
+    public new async Task VerifyEventFormat_Inperson_Filter()
+    {
+        await base.VerifyEventFormat_Inperson_Filter();
+        
+    }
+
+    public new async Task VerifyEventFormat_Online_Filter()
+    {
+        await base.VerifyEventFormat_Online_Filter();
+        
+    }
+
+    public new async Task VerifyEventFormat_Hybrid_Filter()
+    {
+        await base.VerifyEventFormat_Hybrid_Filter();
+        
+    }
+
+    public new async Task VerifyEventRegion_London_Filter()
+    {
+        await base.VerifyEventRegion_London_Filter();
+        
+    }
+}
