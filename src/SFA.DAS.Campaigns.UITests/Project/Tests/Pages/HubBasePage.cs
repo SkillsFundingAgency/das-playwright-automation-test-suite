e925abb3e36b38cf588ecacab939d79cebd185df
Kousik Vedam <146717280+kousikvedam@users.noreply.github.com>
2025-08-08 10:44:53 +0100

    Camp_Fix_Update

--- src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/HubBasePage.cs
+++ src/SFA.DAS.Campaigns.UITests/Project/Tests/Pages/HubBasePage.cs
@@ -14,7 +14,7 @@ public abstract class HubBasePage(ScenarioContext context) : CampaingnsHeaderBas
         {
             try
             {
-                await page.Locator($"a.fiu-card__link:has-text('{fiuCardHeading}')").ClickAsync();
+                await page.Locator("a.fiu-card__link").Filter(new() { HasText = fiuCardHeading }).ClickAsync();
 
                 objectContext.SetDebugInformation($"Clicked fiu card - '{fiuCardHeading}'");
 
