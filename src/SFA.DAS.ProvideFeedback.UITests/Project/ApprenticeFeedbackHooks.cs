5fed1f2ebc3ec7c01a4a05e862d5a9797d768576
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-08-21 18:20:03 +0100

    updated ApprenticeFeedbackHooks

--- src/SFA.DAS.ProvideFeedback.UITests/Project/ApprenticeFeedbackHooks.cs
+++ src/SFA.DAS.ProvideFeedback.UITests/Project/ApprenticeFeedbackHooks.cs
@@ -1,19 +1,17 @@
-﻿//using SFA.DAS.ApprenticeCommitments.APITests.Project.Helpers.SqlDbHelpers;
+﻿namespace SFA.DAS.ProvideFeedback.UITests.Project;
 
-//namespace SFA.DAS.ProvideFeedback.UITests.Project;
+[Binding, Scope(Tag = "apprenticefeedback")]
+public class ApprenticeFeedbackHooks(ScenarioContext context) : BaseHooks(context)
+{
+    private readonly DbConfig _dbConfig = context.Get<DbConfig>();
 
-//[Binding, Scope(Tag = "apprenticefeedback")]
-//public class ApprenticeFeedbackHooks(ScenarioContext context) : BaseHooks(context)
-//{
-//    private readonly DbConfig _dbConfig = context.Get<DbConfig>();
+    [BeforeScenario(Order = 31)]
+    public async Task NavigateToApprenticePortal() => await Navigate(UrlConfig.Apprentice_BaseUrl);
 
-//    [BeforeScenario(Order = 31)]
-//    public void NavigateToApprenticePortal() => _context.Get<TabHelper>().GoToUrl(UrlConfig.Apprentice_BaseUrl);
-
-//    [BeforeScenario(Order = 32)]
-//    public void SetUpHelpers()
-//    {
-//        _context.Set(new ApprenticeCommitmentsSqlDbHelper(_objectContext, _dbConfig));
-//        _context.Set(new ApprenticeCommitmentsAccountsSqlDbHelper(_objectContext, _dbConfig));
-//    }
-//}
+    //[BeforeScenario(Order = 32)]
+    //public void SetUpHelpers()
+    //{
+    //    _context.Set(new ApprenticeCommitmentsSqlDbHelper(_objectContext, _dbConfig));
+    //    _context.Set(new ApprenticeCommitmentsAccountsSqlDbHelper(_objectContext, _dbConfig));
+    //}
+}
