48973dc6e07e6ed5e4aa5aed363bc428abcb6128
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2025-10-22 15:46:28 +0100

    Added a new method for add or search provider page.

---
+++ src/SFA.DAS.AparAdmin.UITests/Project/Tests/Pages/AddOrSearchProviderPage.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.AparAdmin.UITests.Project.Tests.Pages;
+
+public class AddOrSearchProvidersPage(ScenarioContext context) : BasePage(context)
+{
+    public override async Task VerifyPage()
+    {
+        await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Staff dashboard");
+    }
+    public async Task<SFA.DAS.AparAdmin.UITests.Project.Tests.Pages.AddOrSearchProvidersPage> ClickAddOrSearchForProvider()
+    {
+        await page.GetByRole(AriaRole.Link, new() { Name = "Add or search for a provider" }).ClickAsync();
+
+        return await VerifyPageAsync(() => new SFA.DAS.AparAdmin.UITests.Project.Tests.Pages.AddOrSearchProvidersPage(context));
+    }
+}
