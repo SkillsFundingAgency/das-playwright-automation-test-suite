6563267f6ed79ed4135af52edf48519aad929f94
sudhakar.chinoor <sudhakar.chinoor@digital.education.gov.uk>
2025-10-24 16:50:27 +0100

    Updated the steps

--- src/SFA.DAS.AparAdmin.UITests/Project/Tests/Steps/AddTrainingProviderShortCoursesSteps.cs
+++ src/SFA.DAS.AparAdmin.UITests/Project/Tests/Steps/AddTrainingProviderShortCoursesSteps.cs
@@ -10,18 +10,16 @@ public class AddTrainingProviderShortCoursesSteps(ScenarioContext context)
     [Then(@"the user verifies links available in Manage Training Provider page")]
     public async Task ThenTheUserVerifiesLinksAvailableInManageTrainingProviderPage()
     {
-        var page = new ManageTrainingProviderInformationPage(context);
-        
-        var page1 = await page.ClickSearchForATrainingProvider();
+        var page = new AparAdminHomePage(context);
+        await page.ClickAddOrSearchForProvider();
 
+        var managePage = new ManageTrainingProviderInformationPage(context);
+
+        var page1 = await managePage.ClickSearchForATrainingProvider();
         var page2 = await page1.GoBackToManageTrainingProvider();
-
         var page3 = await page2.ClickAddUkprnToAllowList();
-
         var page4 = await page3.GoBackToManageTrainingProvider();
-
         var page5 = await page4.ClickAddNewTrainingProvider();
-
         await page5.GoBackToManageTrainingProvider();
     }
 }
