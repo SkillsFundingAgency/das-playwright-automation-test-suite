458e09a80d66abdbb0a361509218e1d1b37cdad9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-11-26 09:18:16 +0000

    updated NavToHomePage

--- src/SFA.DAS.Approvals.UITests/Project/Pages/ApprovalsBasePage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Pages/ApprovalsBasePage.cs
@@ -1,28 +1,13 @@
-﻿using Azure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SFA.DAS.Approvals.UITests.Project.Pages;
 
-namespace SFA.DAS.Approvals.UITests.Project.Pages
+public abstract class ApprovalsBasePage(ScenarioContext context) : BasePage(context)
 {
-    public abstract class ApprovalsBasePage(ScenarioContext context) : BasePage(context)
-    {
-        public async Task ClickOnNavBarLinkAsync(string linkName)
-        {
-            await page.Locator("#navigation").GetByRole(AriaRole.Link, new() { Name = linkName }).ClickAsync();
-        }
+    public async Task NavToHomePage() => await page.GetByRole(AriaRole.Link, new() { Name = "Home" }).ClickAsync();
 
-        public async Task NavigateBrowserBack()
-        {
-            await page.GoBackAsync();
-        }
+    public async Task NavigateBrowserBack() => await page.GoBackAsync();
 
-        internal async Task ClickOnLink(string linkText) => await page.GetByRole(AriaRole.Link, new() { Name = $"{linkText}", Exact = true }).ClickAsync();
+    internal async Task ClickOnLink(string linkText) => await page.GetByRole(AriaRole.Link, new() { Name = $"{linkText}", Exact = true }).ClickAsync();
 
-        internal async Task ClickOnButton(string linkText) => await page.GetByRole(AriaRole.Button, new() { Name = $"{linkText}"}).ClickAsync();
-
-    }
+    internal async Task ClickOnButton(string linkText) => await page.GetByRole(AriaRole.Button, new() { Name = $"{linkText}" }).ClickAsync();
 
 }
