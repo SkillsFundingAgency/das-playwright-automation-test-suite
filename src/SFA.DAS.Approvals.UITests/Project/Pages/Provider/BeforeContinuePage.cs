b6c29ad4bc5bd5aa002ca69a7432087bba42184e
AshokKumarDfe <ashok.kumar@education.gov.uk>
2025-12-01 12:11:56 +0000

    Added new screen for ILR flow

---
+++ src/SFA.DAS.Approvals.UITests/Project/Pages/Provider/BeforeContinuePage.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.Approvals.UITests.Project.Pages.Provider
+{
+    internal class BeforeContinuePage(ScenarioContext context) : ApprovalsBasePage(context)
+    {
+        private ILocator continueButton => page.GetByRole(AriaRole.Button, new() { Name = "Continue" });
+
+        public override async Task VerifyPage()
+        {
+            await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Before you continue");
+        }
+
+        internal async Task ContinueToChooseACohort()
+        {
+            await continueButton.ClickAsync();
+        }
+    }
+}
