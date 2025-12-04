317129b505959f3e55e52dddcfbd5d89807b671d
Divesh Mavji <divesh987@hotmail.co.uk>
2025-12-04 16:52:19 +0000

    Testing with more logging

--- src/SFA.DAS.FrameworkHelpers/GetSqlConnectionHelper.cs
+++ src/SFA.DAS.FrameworkHelpers/GetSqlConnectionHelper.cs
@@ -7,37 +7,40 @@ public static class GetSqlConnectionHelper
 {
     internal static async Task<SqlConnection> GetSqlConnection(string connectionString)
     {
-        if (connectionString.Contains("Authentication=", StringComparison.OrdinalIgnoreCase))
+    // If connection string has Authentication specified, don't set AccessToken
+    if (connectionString.Contains("Authentication=", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine($"Using connection string authentication");
+        return new SqlConnection(connectionString);
+    }
+
+    var tenantidkey = "TENANTID=";
+    string accessToken;
+
+    if (connectionString.Contains("User ID="))
+    {
+        accessToken = null;
+    }
+    else
+    {
+        if (connectionString.Contains(tenantidkey))
         {
-            return new SqlConnection(connectionString);
-        }
+            var tenantidwithKey = connectionString.Split(';').ToList().Single(x => x.StartsWith(tenantidkey, StringComparison.OrdinalIgnoreCase));
+            connectionString = connectionString.Replace(tenantidwithKey, string.Empty);
+            var tenantid = tenantidwithKey.Replace(tenantidkey, string.Empty);
 
-        var tenantidkey = "TENANTID=";
-
-        string accessToken;
-
-        if (connectionString.Contains("User ID="))
-        {
-            accessToken = null;
+            Console.WriteLine($"Using tenant ID from connection string: {tenantid}");
+            accessToken = await AzureTokenService.GetDatabaseAuthToken(tenantid);
         }
         else
         {
-            if (connectionString.Contains(tenantidkey))
-            {
-                var tenantidwithKey = connectionString.Split(';').ToList().Single(x => x.StartsWith(tenantidkey, StringComparison.OrdinalIgnoreCase));
-
-                connectionString = connectionString.Replace(tenantidwithKey, string.Empty);
-
-                var tenantid = tenantidwithKey.Replace(tenantidkey, string.Empty);
-
-                accessToken = await AzureTokenService.GetDatabaseAuthToken(tenantid);
-            }
-            else
-            {
-                accessToken = await AzureTokenService.GetDatabaseAuthToken();
-            }
+            Console.WriteLine($"Using default tenant for token");
+            accessToken = await AzureTokenService.GetDatabaseAuthToken();
         }
 
-        return new() { ConnectionString = connectionString, AccessToken = accessToken };
+        Console.WriteLine($"Access token acquired: {(string.IsNullOrEmpty(accessToken) ? "NO" : "YES")}");
+    }
+
+    return new() { ConnectionString = connectionString, AccessToken = accessToken };
     }
 }
