3482c07b7aa1967040b0f5c7718f02e0303eb6db
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-12-16 16:24:03 +0000

    updated ViewApplication

--- src/SFA.DAS.FAA.UITests/Project/Tests/Pages/FAA_ApplicationsPage.cs
+++ src/SFA.DAS.FAA.UITests/Project/Tests/Pages/FAA_ApplicationsPage.cs
@@ -30,7 +30,7 @@ public class FAA_ApplicationsPage(ScenarioContext context) : FAABasePage(context
 
     public async Task ViewApplication()
     {
-        await page.GetByRole(AriaRole.Link, new() { Name = vacancyTitleDataHelper.VacancyTitle, Exact = true }).ClickAsync();
+        await page.GetByRole(AriaRole.Link, new() { Name = vacancyTitleDataHelper.VacancyTitle}).ClickAsync();
 
         await page.GetByRole(AriaRole.Link, new() { Name = $"View application   for {vacancyTitleDataHelper.VacancyTitle}" }).ClickAsync();
 
