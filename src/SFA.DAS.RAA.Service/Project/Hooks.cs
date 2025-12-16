5fce5ed5a94d6214e467bd9e2947509c63394ea9
Arvinth Seran <arvinth.seran@digital.education.gov.uk>
2025-12-16 12:16:31 +0000

    fixed config error

--- src/SFA.DAS.RAA.Service/Project/Hooks.cs
+++ src/SFA.DAS.RAA.Service/Project/Hooks.cs
@@ -1,5 +1,4 @@
 ﻿
-
 using SFA.DAS.RAA.Service.Project.Helpers;
 
 namespace SFA.DAS.RAA.Service.Project;
@@ -8,7 +7,7 @@ namespace SFA.DAS.RAA.Service.Project;
 public class Hooks(ScenarioContext context)
 {
     [BeforeScenario(Order = 33)]
-    public void SetUpHelpers()
+    public async Task SetUpHelpers()
     {
         var vacancyTitleDatahelper = context.Get<VacancyTitleDatahelper>();
 
@@ -25,5 +24,13 @@ public class Hooks(ScenarioContext context)
         context.Set(new ProviderCreateVacancySqlDbHelper(objectContext, dbConfig));
 
         context.Set(new RAAProviderPermissionsSqlDbHelper(objectContext, dbConfig));
+
+        var page = context.Get<Driver>().Page;
+
+        page.SetDefaultNavigationTimeout(10000);
+
+        page.SetDefaultTimeout(15000);
+
+        await Task.CompletedTask;
     }
 }
--- src/SFA.DAS.RAAEmployer.UITests/Project/Hooks.cs
+++ src/SFA.DAS.RAAEmployer.UITests/Project/Hooks.cs
@@ -1,21 +1,13 @@
-﻿namespace SFA.DAS.RAAEmployer.UITests.Project;
+﻿//namespace SFA.DAS.RAAEmployer.UITests.Project;
 
-[Binding]
-public class Hooks(ScenarioContext context)
-{
-    [BeforeScenario(Order = 34)]
-    public async Task SetUpHelpers()
-    {
-        //var apprenticeCourseDataHelper = new ApprenticeCourseDataHelper(new RandomCourseDataHelper(), ApprenticeStatus.WaitingToStart, []);
+//[Binding]
+//public class Hooks(ScenarioContext context)
+//{
+//    [BeforeScenario(Order = 34)]
+//    public async Task SetUpHelpers()
+//    {
+//        var apprenticeCourseDataHelper = new ApprenticeCourseDataHelper(new RandomCourseDataHelper(), ApprenticeStatus.WaitingToStart, []);
 
-        //context.Set(apprenticeCourseDataHelper);
-
-        var page = context.Get<Driver>().Page;
-
-        page.SetDefaultNavigationTimeout(10000);
-
-        page.SetDefaultTimeout(15000);
-
-        await Task.CompletedTask;
-    }
-}
+//        context.Set(apprenticeCourseDataHelper);
+//    }
+//}
