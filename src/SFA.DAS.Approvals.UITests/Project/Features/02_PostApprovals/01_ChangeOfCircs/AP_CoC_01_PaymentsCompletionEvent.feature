88a995cc83d3377555049d9e363344ad9864d25c
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2025-12-22 10:20:07 +0000

    working prototype

---
+++ src/SFA.DAS.Approvals.UITests/Project/Features/02_PostApprovals/01_ChangeOfCircs/AP_CoC_01_PaymentsCompletionEvent.feature
@@ -0,0 +1,8 @@
+﻿Feature: AP_CoC_01_PaymentsCompletionEvent
+
+A short summary of the feature
+
+@tag1
+Scenario: AP_CoC_01_Verify Payment Completion event marks the apprenticeship as Complete
+	Given test123
+
---
+++ src/SFA.DAS.Approvals.UITests/Project/Steps/ServiceBusEventsRelatedSteps.cs
@@ -0,0 +1,41 @@
+﻿using SFA.DAS.FrameworkHelpers;
+using SFA.DAS.LearnerData.Events;
+using SFA.DAS.Payments.ProviderPayments.Messages;
+using System;
+using System.Text.Json;
+
+
+namespace SFA.DAS.Approvals.UITests.Project.Steps
+{
+    [Binding]
+    public class ServiceBusEventsRelatedSteps
+    {
+        private readonly ScenarioContext context;
+        private readonly ObjectContext objectContext;
+
+        public ServiceBusEventsRelatedSteps(ScenarioContext _context)
+        {
+            context = _context;
+            objectContext = context.Get<ObjectContext>();
+        }
+
+
+        [Given("test123")]
+        public async Task GivenTest123()
+        {
+            await PublishRecordedAct1CompletionPaymentEvent(204782);
+        }
+
+        public async Task PublishRecordedAct1CompletionPaymentEvent(int apprenticeshipId)
+        {
+            var pymtCompletionEvent = new RecordedAct1CompletionPayment { ApprenticeshipId = apprenticeshipId, EventTime = DateTimeOffset.UtcNow };
+            var serviceBusHelper = GlobalTestContext.ServiceBus;
+            await serviceBusHelper.Publish(pymtCompletionEvent);
+
+            objectContext.SetDebugInformation($"Publishing Payment Completion Event (RecordedAct1CompletionPayment) to N-Service Bus for following apprentice:");
+            objectContext.SetDebugInformation(JsonSerializer.Serialize(pymtCompletionEvent, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+
+    }
+}
