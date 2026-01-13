2aeadd649872dc969f284cd37b0aa22c190233be
CHINOOR, Sudhakar <Sudhakar.CHINOOR@EDUCATION.GOV.UK>
2026-01-13 15:33:55 +0000

    Added new tests

---
+++ src/SFA.DAS.AparAdmin.UITests/Project/Tests/Pages/AddJourney/ApprenticeshipsUnitsPage.cs
@@ -0,0 +1,16 @@
+﻿namespace SFA.DAS.AparAdmin.UITests.Project.Tests.Pages.AddJourney;
+
+public class ApprenticeshipsUnitsPage(ScenarioContext context) : BasePage(context)
+{
+    public override async Task VerifyPage()
+    {
+        await Assertions.Expect(page.Locator("h1"))
+            .ToContainTextAsync("Do they offer apprenticeship units?");
+    }
+    public async Task<OrganisationsPage> YesOfferApprenticeshipUnits()
+    {
+        await page.GetByLabel("Yes").CheckAsync();
+        await page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+        return await VerifyPageAsync(() => new OrganisationsPage(context));
+    }
+}
