3fb2ae8eb3ac303aea7c98215359affb586760ae
Najamuddin-Muhammad <najamuddin.muhammad@education.gov.uk>
2026-03-16 11:12:45 +0000

    updated sql query

--- src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/LearningDbSqlHelper.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Helpers/SqlHelpers/LearningDbSqlHelper.cs
@@ -6,7 +6,11 @@
 
         internal async Task<string> CheckIfApprenticeshipRecordCreatedInLearningDb(int ApprenticeshipId, string ULN)
         {
-            string query = $"SELECT [Key] FROM [dbo].[Learning] WHERE ApprovalsApprenticeshipId = {ApprenticeshipId} AND Uln = {ULN}";
+            string query = $@"SELECT * FROM [dbo].[Learner] l
+                                JOIN [dbo].[ApprenticeshipLearning] al 
+                                ON l.[key] = al.LearnerKey
+                                WHERE l.uln = {ULN}
+                                AND al.ApprovalsApprenticeshipId = {ApprenticeshipId}";
             var result = await GetData(query);
             return result.FirstOrDefault() ?? string.Empty;
         }
