f0814a5e5d7a8e261b8d1e745085bfd36af3889e
REDDY, Harsha <Harsha.REDDY@EDUCATION.GOV.UK>
2026-05-20 16:25:14 +0100

    update Hooks

--- src/SFA.DAS.RAA.Service/Project/Hooks.cs
+++ src/SFA.DAS.RAA.Service/Project/Hooks.cs
@@ -1,5 +1,4 @@
 ﻿
-using SFA.DAS.RAA.DataGenerator.Project.Helpers;
 using SFA.DAS.RAA.Service.Project.Helpers;
 
 namespace SFA.DAS.RAA.Service.Project;
@@ -7,63 +6,35 @@ namespace SFA.DAS.RAA.Service.Project;
 [Binding]
 public class Hooks(ScenarioContext context)
 {
-    [BeforeScenario(Order = 32)]
-    public void SetUpDataHelpers()
-    {
-        var objectContext = context.Get<ObjectContext>();
-
-        objectContext.SetConsoleAndDebugInformation("Entered SetUpDataHelpers Order = 32 hook");
-
-        var vacancyTitleDatahelper = new VacancyTitleDatahelper(isCloneVacancy: false);
-        context.Set(vacancyTitleDatahelper);
-    }
-
     [BeforeScenario(Order = 33)]
     public async Task SetUpHelpers()
     {
-        try
-        {
-            var objectContext = context.Get<ObjectContext>();
+        var vacancyTitleDatahelper = context.Get<VacancyTitleDatahelper>();
 
-            objectContext.SetConsoleAndDebugInformation("Entered SetUpHelpers Order = 33 hook");
+        var fAAConfig = context.GetFAAConfig<FAAUserConfig>();
 
-            var vacancyTitleDatahelper = context.Get<VacancyTitleDatahelper>();
+        context.Set(new RAADataHelper(fAAConfig, vacancyTitleDatahelper));
 
-            var fAAConfig = context.GetFAAConfig<FAAUserConfig>();
+        context.Set(new AdvertDataHelper());
 
-            context.Set(new RAADataHelper(fAAConfig, vacancyTitleDatahelper));
+        var dbConfig = context.Get<DbConfig>();
 
-            context.Set(new AdvertDataHelper());
+        var objectContext = context.Get<ObjectContext>();
 
-            var dbConfig = context.Get<DbConfig>();
+        context.Set(new ProviderCreateVacancySqlDbHelper(objectContext, dbConfig));
 
-            objectContext.SetConsoleAndDebugInformation("Setting ProviderCreateVacancySqlDbHelper");
-            context.Set(new ProviderCreateVacancySqlDbHelper(objectContext, dbConfig));
+        context.Set(new RAAProviderPermissionsSqlDbHelper(objectContext, dbConfig));
 
-            objectContext.SetConsoleAndDebugInformation("Setting RAAProviderPermissionsSqlDbHelper");
-            context.Set(new RAAProviderPermissionsSqlDbHelper(objectContext, dbConfig));
+        var browserContext = context.Get<Driver>().BrowserContext;
 
-            objectContext.SetConsoleAndDebugInformation("Configuring browser timeouts");
-            var browserContext = context.Get<Driver>().BrowserContext;
+        objectContext.SetDebugInformation("*****Setting DefaultNavigationTimeout to be 40000ms = 40 sec*****");
 
-            objectContext.SetDebugInformation("*****Setting DefaultNavigationTimeout to be 40000ms = 40 sec*****");
+        browserContext.SetDefaultNavigationTimeout(40000);
 
-            browserContext.SetDefaultNavigationTimeout(40000);
+        objectContext.SetDebugInformation("*****Setting DefaultTimeout to be 40000ms = 40 sec*****");
 
-            objectContext.SetDebugInformation("*****Setting DefaultTimeout to be 40000ms = 40 sec*****");
-
-            browserContext.SetDefaultTimeout(40000);
-
-            objectContext.SetConsoleAndDebugInformation("Completed SetUpHelpers Order = 33 hook");
-        }
-        catch (Exception ex)
-        {
-            var objectContext = context.Get<ObjectContext>();
-            objectContext.SetConsoleAndDebugInformation($"ERROR in SetUpHelpers Order = 33 hook: {ex.Message}");
-            throw;
-        }
+        browserContext.SetDefaultTimeout(40000);
 
         await Task.CompletedTask;
     }
 }
-
