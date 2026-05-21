98e2e6625b7cbabe725ed606735dfc07f16d5f18
sony-kotha <sony.kotha@education.gov.uk>
2026-05-21 17:29:14 +0100

    PageTitleChange

--- src/SFA.DAS.Approvals.UITests/Project/Pages/Provider/ConfirmYourReservationPage.cs
+++ src/SFA.DAS.Approvals.UITests/Project/Pages/Provider/ConfirmYourReservationPage.cs
@@ -4,7 +4,7 @@
     {
         public override async Task VerifyPage()
         {
-            await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Confirm your reservation");
+            await Assertions.Expect(page.Locator("h1")).ToContainTextAsync("Check and confirm your reservation");
         }
 
         internal async Task<YouHaveReservedFundingForTrainingPage> ClickConfirmButton()
