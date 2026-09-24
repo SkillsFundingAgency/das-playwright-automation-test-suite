14c73e0b034e69da65ffe97851d1424bcb8bd52c
ASelva-EDU <arunkumar.selvasundaram@education.gov.uk>
2026-09-24 13:35:56 +0100

    Locators are updated.

--- src/SFA.DAS.Digicerts.UITests/Project/Tests/Pages/Authorisation/DigiCertsAuthorisationLearnerPage.cs
+++ src/SFA.DAS.Digicerts.UITests/Project/Tests/Pages/Authorisation/DigiCertsAuthorisationLearnerPage.cs
@@ -26,8 +26,8 @@ namespace SFA.DAS.Digicerts.UITests.Project.Tests.Pages.Authorisation
         {
             Console.WriteLine($"Auth5");
             await page.GetByRole(AriaRole.Radio, new() { Name = "Yes" }).CheckAsync();
-            Console.WriteLine($"Auth6");
-            await page.GetByRole(AriaRole.Spinbutton, new() { Name = "Unique learner number" }).FillAsync(uln);
+            Console.WriteLine($"Auth6"+ uln);
+            await page.GetByRole(AriaRole.Textbox, new() { Name = "Unique learner number" }).FillAsync(uln);
 
             await page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
 
